e226bf1a8c7003ce20f24b9f8b11af7119a1fd75
Lucas Peil <55022960+LucasPeil@users.noreply.github.com>
2022-09-02 01:05:57 -0300

    Add files via upload

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,23 @@
 
+import { useState } from 'react';
 import './App.css';
 import DessertForm from './components/DessertForm';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+import {IDesserts} from "./interfaces/Desserts"
+
 function App() {
+
+  const [dessertList, setDessertList] = useState<IDesserts[]>([])
+  const [updatedDessert, setUpdatedDessert] = useState<IDesserts | null>(null)
+ 
   return (
     <div className="App">
       <Header/>
       <div>
         <h2>Qual sobremesa você quer fazer hoje?</h2>
-        <DessertForm btnText = "Adicionar"/>
+        <DessertForm btnText = "Adicionar" dessertList={dessertList} setDessertList={setDessertList}/>
       </div>
       <div>
         <h2>Sobremesas:</h2>
--- Frontend/src/components/DessertForm.tsx
+++ Frontend/src/components/DessertForm.tsx
@@ -1,21 +1,50 @@
 
-import React from 'react'
+import React, {useState,useEffect,ChangeEvent,FormEvent} from 'react'
+import {IDesserts} from "../interfaces/Desserts"
+
 
 interface Props {
     btnText: string
+    dessertList: IDesserts[]
+    setDessertList?: React.Dispatch<React.SetStateAction<IDesserts[]>>
 }
 
-const DesertForm = ({btnText}:Props) => {
+const DesertForm = ({btnText, dessertList, setDessertList}:Props) => {
+
+    const [id, setId] = useState<number>(0)
+    const [name,setName] = useState<string>("")
+    const [difficulty,setDifficulty] = useState<number >(0)
+
+    const addDessertHandler = (e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        const id:number = Math.floor(Math.random()*1000)
+        const newDessert: IDesserts = {id,name,difficulty}
+        setDessertList!([...dessertList, newDessert])
+
+        setName("")
+        setDifficulty(0)
+        
+        console.log(dessertList)
+    }
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) =>{
+        if(e.target.name === "name"){
+            setName(e.target.value)
+        }else{
+            setDifficulty(parseInt(e.target.value))
+        }
+
+    }
   return (
     <div>
-        <form >
+        <form onSubmit={addDessertHandler}>
             <div>
                 <label htmlFor="name">Sobremesa</label>
-                <input type="text" name="name" placeholder="Ex:Pudim" />
+                <input type="text" name="name" placeholder="Ex:Pudim" onChange={onChangeHandler} value={name} />
             </div>
             <div>
                 <label htmlFor="difficulty">Dificuldade</label>
-                <input type="number" name="difficulty" placeholder=" Dificuldade de 1 ( muito fácil) à 5 (muito difícil)" />
+                <input type="number" name="difficulty" placeholder=" Dificuldade de 1 ( muito fácil) à 5 (muito difícil)" 
+                 onChange={onChangeHandler} value={difficulty}/>
             </div>
             <div>
                 <input type="submit" value={btnText} />
